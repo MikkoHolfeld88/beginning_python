0e814010552c4534e7c6b38ae0704cd50c332571
MikkoTheOne <mikko.holfeld@gmx.de>
2019-09-30 12:18:32 +0200

    Oval tkinter, kivy

--- tkinter/oval.py
+++ tkinter/oval.py
@@ -6,24 +6,13 @@ from random import randint
 Creating MainWindow App runs on
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-mainWindow = Tk()
+root = tk.Tk()
 
-screen_width = mainWindow.winfo_screenwidth()
-screen_height = mainWindow.winfo_screenheight()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-mainWindow.minsize(int(screen_width/2),int(screen_height/2))
-mainWindow.title("Circle")
-
-canvas = Canvas(mainWindow,width=screen_width,height=screen_height)
-
-canvas.pack()
-
-class Circle():
-
-    def __init__(x,y,size,color):
-        canvas.create_oval(x,y,size,size,color)
-
-Kreis = Circle(45,45,56)
+root.minsize(int(screen_width/2),int(screen_height/2))
+root.title("Elliot")
 
 mainloop()
 
